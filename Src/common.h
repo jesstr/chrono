d6581adc50e37d2f74173292e16ceda86e547963
jesstr <jesstr@mail.ru>
2022-02-07 18:59:04 +0300

    Added isInterrupt() macro

--- Src/common.h
+++ Src/common.h
@@ -7,4 +7,6 @@
 #define MAX(x , y)  (((x) > (y)) ? (x) : (y))
 #define array_len(x) (sizeof(x)/sizeof(x[0]))
 
+#define isInterrupt() ((SCB->ICSR & SCB_ICSR_VECTACTIVE_Msk) != 0)
+
 #endif /* __COMMON_H__ */
